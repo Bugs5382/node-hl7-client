09dee8cd1980dcb8cd8c557cb1b5c78931a91dba
Shane Froebel <bugs5382@users.noreply.github.com>
2023-11-18 11:24:34 -0500

    feat: added parser within the client package
    #4 [ci skip]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Client} from "./client.js";
+import {Parser} from "./parser.js";
 
 export default Client
-export { Client }
+export { Client, Parser }
 
 export type {ConnectionOptions} from './normalize.js'
