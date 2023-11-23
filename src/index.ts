de3e81a432cb4a42695a1c21a1dabf609694a447
Shane Froebel <bugs5382@users.noreply.github.com>
2023-11-22 20:41:36 -0500

    feat: documentation updates
    #4

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Client } from './client.js'
 import { Listener } from './listener.js'
-import { Parser } from './parser.js'
+import {ISegment, Parser } from './parser.js'
 import { Segment } from './segment.js'
 
 export default Client
-export { Client, Listener, Parser, Segment }
+export { Client, Listener, Parser, Segment, ISegment }
 
 export type { ClientOptions, ClientListenerOptions, ParserOptions, ParserProcessRawData } from './normalize.js'
 export type { HL7ClientError, HL7ClientFatalError } from './exception.js'
