022486ce4193caa4c8ed786d9bd6eb180dc9bdb0
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-02 09:51:36 -0500

    feat(experiential): re-doing the code
    #4 [ci skip]

---
+++ src/decorators/interfaces/node.ts
@@ -0,0 +1,23 @@
+export interface Node {
+  name: string;
+  length: number;
+
+  get(path: string | number): Node;
+  set(path: string | number, value?: any): Node;
+
+  exists(path: string | number): boolean;
+  forEach(callback: (value: Node, index: number) => void): void;
+
+  toString(): string;
+  toRaw(): string;
+  toArray(): Node[];
+  isEmpty(): boolean;
+  toDate(): Date;
+  toInteger(): number;
+  toFloat(): number;
+  toBoolean(): boolean;
+
+  read(path: string[]): Node;
+  write(path: string[], value: string): Node;
+  path: string[];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { Client } from './client.js'
 import { Listener } from './listener.js'
 import { Message } from './message.js'
-import { Node } from './node.js'
 import { Parser, ParserPlan } from './parser.js'
-/** HL7 Specs **/
-
 
 export default Client
-export { Client, Listener, Parser, ParserPlan, Message, Node }
+export { Client, Listener, Parser, ParserPlan, Message }
 
-export type { HL7_2_7_MSH, HL7_2_7} from './specification/2.7.js'
+/** HL7 Specs **/
+export type { HL7_2_7_MSH, HL7_2_7 } from './specification/2.7.js'
 
-export type { ClientOptions, ClientListenerOptions, ParserProcessRawData } from './normalize.js'
+export type { ClientOptions, ClientListenerOptions, ClientBuilderOptions, ClientBuilderBatchOptions, ClientBuilderFileOptions, ParserProcessRawData} from './normalize.js'
 export type { HL7Error, HL7FatalError, HL7ParserError } from './exception.js'
