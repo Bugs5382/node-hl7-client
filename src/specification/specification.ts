e20ccedbf515087081daf7f18aaac3b05583e912
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-02 12:09:07 -0500

    feat(experiential): some unit tests passing
    #4 doesn't generate a message, but basic unit testing is being worked on [ci skip]

--- src/specification/specification.ts
+++ src/specification/specification.ts
@@ -5,12 +5,14 @@ export type MSH = HL7_2_7_MSH
 
 /** @internal */
 export interface HL7_SPEC {
-  checkMSH: (options: MSH) => MSH
+  name: string;
+  checkMSH: (options: MSH) => boolean
 }
 
 /** @internal */
 export class HL7_SPEC_BASE implements HL7_SPEC {
-  checkMSH (_options: MSH): MSH {
+  name = ""
+  checkMSH (_options: MSH): boolean {
     throw new Error('Not Implemented')
   }
 }
