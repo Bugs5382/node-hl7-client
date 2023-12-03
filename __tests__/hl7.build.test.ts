082e90eb24e8db191f6d5b8a0f488c934021b84e
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-03 09:18:58 -0500

    feat(experiential): unit test failing, but showing promising results
    #4 with the current uncommitted code base, this unit test shows the initial builder code getting close to working. this unit test should pass once the code is working correctly [ci skip]

--- __tests__/hl7.build.test.ts
+++ __tests__/hl7.build.test.ts
@@ -1,3 +1,4 @@
+import {randomUUID} from "crypto";
 import {Message} from "../src";
 
 describe('node hl7 client - builder tests', () => {
@@ -34,12 +35,14 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 9.1 is empty ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
-          msh_9: {
-            // @ts-expect-error 9.1 should be not empty
-            msh_9_1: "",
-          },
-        }})
+        const message = new Message({
+          mshHeader: {
+            msh_9: {
+              // @ts-expect-error 9.1 should be not empty
+              msh_9_1: "",
+            },
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.1 & MSH 9.2 & MSH 9.3 must be defined.'))
       }
@@ -48,12 +51,14 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 9.2 is empty ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               // @ts-expect-error 9.2 should be not empty
               msh_9_2: "",
             },
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.1 & MSH 9.2 & MSH 9.3 must be defined.'))
       }
@@ -62,12 +67,14 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 9.3 is empty ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               // @ts-expect-error 9.3 should be not empty
               msh_9_3: "",
             },
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.1 & MSH 9.2 & MSH 9.3 must be defined.'))
       }
@@ -76,14 +83,16 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 9.1 is not 3 character long ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               // @ts-expect-error 9.1 should be 3 characters
               msh_9_1: "ADTY",
               msh_9_2: "A01",
               msh_9_3: "ADT_A01"
             },
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.1 must be 3 characters in length.'))
       }
@@ -92,46 +101,33 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 9.2 is not 3 character long ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               msh_9_1: "ADT",
               // @ts-expect-error 9.2 should be 3 characters
               msh_9_2: "A01Y",
               msh_9_3: "ADT_A01"
             },
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.2 must be 3 characters in length.'))
-      }
-    })
-
-    test("error - Message Object - msh 9.3 is not 7 character long ", async () => {
-      try {
-        // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
-            msh_9: {
-              msh_9_1: "ADT",
-              msh_9_2: "A01",
-              // @ts-expect-error 9.3 should 7 characters
-              msh_9_3: "ADT_A01Y"
-            },
-          }})
-      } catch (err) {
-        expect(err).toEqual(new Error('MSH.9.3 must be 7 characters in length.'))
       }
     })
 
     test("error - Message Object - msh 10 is more than 199 characters ", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               msh_9_1: "ADT",
-              msh_9_2: "A01",
-              msh_9_3: "ADT_A01"
+              msh_9_2: "A01"
             },
             msh_10: "AaasdfjlsdjflskdfsdflkjjsdlkflkasdflsjdflkjasdlfjsldkjlskjdfksajflksjdfAaasdfjlsdjflskdfsdflkjjsdlkflkasdflsjdflkjasdlfjsldkjlskjdfksajflksjdfAaasdfjlsdjflskdfsdflkjjsdlkflkasdflsjdflkjasdlfjsldkjlskjdfksajflksjdf"
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.10 must be greater than 0 and less than 199 characters.'))
       }
@@ -140,17 +136,40 @@ describe('node hl7 client - builder tests', () => {
     test("error - Message Object - msh 10 can not be blank", async () => {
       try {
         // @ts-expect-error Message is not used. That's fine.
-        const message = new Message({mshHeader: {
+        const message = new Message({
+          mshHeader: {
             msh_9: {
               msh_9_1: "ADT",
-              msh_9_2: "A01",
-              msh_9_3: "ADT_A01"
+              msh_9_2: "A01"
             },
             msh_10: ""
-          }})
+          }
+        })
       } catch (err) {
         expect(err).toEqual(new Error('MSH.9.10 must be greater than 0 and less than 199 characters.'))
       }
+    })
+
+  })
+
+  describe(`'build basics`, () => {
+
+    test("simple message with just MSH header", async () => {
+
+      const randomControlID = randomUUID()
+
+      const message = new Message({
+        mshHeader: {
+            msh_9: {
+              msh_9_1: "ADT",
+              msh_9_2: "A01"
+            },
+            msh_10: randomControlID
+          }
+      })
+      expect(message.toString()).toContain("MSH|^~\\&")
+      expect(message.toString()).toContain(`|ADT^A01^ADT_A01|${randomControlID}||2.7`)
+
     })
 
   })
