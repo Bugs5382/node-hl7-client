7f21da7bbad64828c65814d16ba402963c98d005
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-03 11:27:34 -0500

    feat(experiential):typescript validation x4 // hide inherited from typedoc by default
    #4 [ci skip]

--- src/valueNode.ts
+++ src/valueNode.ts
@@ -1,67 +1,60 @@
-import Node from "./node";
-import NodeBase from './nodeBase';
-import Delimiters from "./delimiters";
+import {Delimiters} from "./decorators/enum/delimiters";
+import {NodeBase} from "./nodeBase";
 
-export default class ValueNode extends NodeBase {
+/** @internal */
+export class ValueNode extends NodeBase {
 
     protected key: string;
 
-    constructor(parent: NodeBase, key: string, text: string, delimiter: Delimiters = null) {
+    constructor(parent: NodeBase, key: string, text: string, delimiter: Delimiters | undefined = undefined) {
         super(parent, text, delimiter);
-
         this.key = key;
     }
 
     toString(): string {
-
-        var children = this.children;
+        let children = this.children;
         return children.length > 1 ? this.toRaw() : children[0].toString();
     }
 
     toDate(): Date {
-
-        var text = this.toString();
-
+        let text = this.toString();
         if(text.length == 8) {
-            // YYYYMMDD
+            /** YYYYMMDD */
             return new Date(parseInt(text.slice(0, 4)), parseInt(text.slice(4, 6)) - 1, parseInt(text.slice(6, 8)), 0, 0, 0);
         }
         else if(text.length == 12) {
-            // YYYYMMDDHHMM
+            /** YYYYMMDDHHMM */
             return new Date(parseInt(text.slice(0, 4)), parseInt(text.slice(4, 6)) - 1, parseInt(text.slice(6, 8)), parseInt(text.slice(8, 10)), parseInt(text.slice(10, 12)), 0);
         }
         else if(text.length >= 14) {
-            // YYYYMMDDHHMMSS
+            /** YYYYMMDDHHMMSS **/
             return new Date(parseInt(text.slice(0, 4)), parseInt(text.slice(4, 6)) - 1, parseInt(text.slice(6, 8)), parseInt(text.slice(8, 10)), parseInt(text.slice(10, 12)), parseInt(text.slice(12, 14)));
         }
-
-        return null;
+        throw new Error("Invalid Date Format")
     }
 
     toInteger(): number {
-
         return parseInt(this.toString());
     }
 
     toFloat(): number {
-
         return parseFloat(this.toString());
     }
 
     toBoolean(): boolean {
-
         switch(this.toString()) {
             case "Y":
                 return true;
             case "N":
                 return false;
         }
-
-        return null;
+        throw new Error("Not a valid value for boolean value.")
     }
 
     protected pathCore(): string[] {
-
+        if (this.parent === null) {
+            throw new Error("Somehow, this.parent is null.")
+        }
         return this.parent.path.concat([this.key]);
     }
 }
