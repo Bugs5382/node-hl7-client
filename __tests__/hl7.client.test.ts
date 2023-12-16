e38d8087dfa10f6eb7495eca052ea9cdd66ed2bb
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-16 11:26:01 -0500

    build: added in node-hl7-server
    * added in dev dependency to NPM reg

--- __tests__/hl7.client.test.ts
+++ __tests__/hl7.client.test.ts
@@ -1,9 +1,9 @@
-// import fs from "fs";
-// import path from "node:path";
-// import portfinder from 'portfinder'
-// import {Hl7Inbound, Server} from "../../node-hl7-server/src";
-// import {Client, expectEvent, HL7Outbound, Message, sleep} from '../src'
-import {Client} from '../src'
+ import fs from "fs";
+ import path from "node:path";
+ import portfinder from 'portfinder'
+ import {Hl7Inbound, Server} from "node-hl7-server";
+ import {Client, HL7Outbound, Message} from '../src'
+ import {expectEvent, sleep} from "./__utils__";
 
 describe('node hl7 client', () => {
 
@@ -111,7 +111,7 @@ describe('node hl7 client', () => {
 
   })
 
-  /*test('...simple connect', async () => {
+  test('...simple connect', async () => {
 
       const LISTEN_PORT = await portfinder.getPortPromise({
         port: 3000,
@@ -130,9 +130,9 @@ describe('node hl7 client', () => {
       await outGoing.close()
       await listener.close()
 
-    })*/
+    })
 
-  /*describe('server/client sanity checks', () => {
+  describe('server/client sanity checks', () => {
 
     let waitAck: number = 0
 
@@ -223,9 +223,9 @@ describe('node hl7 client', () => {
 
     })
 
-  })*/
+  })
 
-  /*describe('end to end tests', () => {
+  describe('end to end tests', () => {
 
     describe('...send message, get proper ACK', () => {
 
@@ -359,6 +359,6 @@ describe('node hl7 client', () => {
       }
     })
 
-  })*/
+  })
 
 })
