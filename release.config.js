1a2f6442843447ed104a75ba2c57df1ecd830817
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-18 13:05:26 -0500

    chore: standerized semantic-release
    * added  @the-rabbit-hole/semantic-release-config as part of the semantic-release config
    * ensure projects are all following the same pattern
    
    [ci skip]

--- release.config.js
+++ release.config.js
@@ -1,25 +1,7 @@
 module.exports = {
+    "extends": "@the-rabbit-hole/semantic-release-config",
     "branches": [
         "main",
         { name: 'develop', prerelease: 'beta' }
-    ],
-    "verifyConditions": ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/git'],
-    "plugins": [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/changelog',
-            {
-                "changelogFile": "CHANGELOG.md"
-            }
-        ], [
-            "@semantic-release/git",
-            {
-                "assets": ["package.json", "CHANGELOG.md"],
-                "message": "chore(release): ${nextRelease.version} [ci skip]\n\n${nextRelease.notes}"
-            }
-        ],
-        "@semantic-release/github",
-        "@semantic-release/npm"
     ]
 }
