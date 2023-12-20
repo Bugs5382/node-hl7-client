e1c81d0e4f4c9b5ba8bcbf4c3f576b7e84cfd73e
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-20 13:32:10 -0500

    docs: move HL7 Specs
    * moved HL7 its own file
    * removed the modules from 'builder' as they should never be used directly
    * updated entry points
    
    [ci skip]

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,18 @@
-import { NodeBase } from './builder/modules/nodeBase.js'
-import { EmptyNode } from './builder/modules/emptyNode.js'
-import { ValueNode } from './builder/modules/valueNode.js'
-import { RootBase } from './builder/modules/rootBase.js'
-import { Node } from './builder/interface/node.js'
 import { Client } from './client/client.js'
-import { Component } from './builder/modules/component.js'
-import { Field } from './builder/modules/field.js'
-import { FieldRepetition } from './builder/modules/fieldRepetition.js'
 import { Message } from './builder/message.js'
-import { Segment } from './builder/modules/segment.js'
-import { SegmentList } from './builder/modules/segmentList.js'
-import { SubComponent } from './builder/modules/subComponent.js'
 import { Batch } from './builder/batch.js'
 import { FileBatch } from './builder/fileBatch.js'
-import { ParserPlan } from './utils/parserPlan.js'
 import { HL7Outbound } from './client/hl7Outbound.js'
-import { Delimiters, ReadyState } from './utils/enum.js'
-import { HL7_2_1, HL7_2_1_MSH } from './specification/2.1.js'
-import { HL7_2_2, HL7_2_2_MSH } from './specification/2.2.js'
-import { HL7_2_3, HL7_2_3_MSH } from './specification/2.3.js'
-import { HL7_2_3_1, HL7_2_3_1_MSH } from './specification/2.3.1.js'
-import { HL7_2_4, HL7_2_4_MSH } from './specification/2.4.js'
-import { HL7_2_5, HL7_2_5_MSH } from './specification/2.5.js'
-import { HL7_2_5_1, HL7_2_5_1_MSH } from './specification/2.5.1.js'
-import { HL7_2_6, HL7_2_6_MSH } from './specification/2.6.js'
-import { HL7_2_7, HL7_2_7_MSH } from './specification/2.7.js'
-import { HL7_2_7_1, HL7_2_7_1_MSH } from './specification/2.7.1.js'
-import { HL7_2_8, HL7_2_8_MSH } from './specification/2.8.js'
-import { HL7_SPEC, HL7_SPEC_BASE } from './specification/specification.js'
+import { ReadyState } from './utils/enum.js'
 import { OutboundHandler } from './utils/normalizedClient.js'
 import { InboundResponse } from './client/module/inboundResponse.js'
 export { MSH } from './specification/specification.js'
 
 export { expBackoff, assertNumber, isHL7Number, isHL7String, validIPv4, validIPv6, createHL7Date, isBatch, isFile, padHL7Date, escapeForRegExp, decodeHexString, randomString } from './utils/utils.js'
 
-/** HL7 Class **/
-export {
-  HL7_2_1, HL7_2_1_MSH,
-  HL7_2_2, HL7_2_2_MSH,
-  HL7_2_3, HL7_2_3_MSH,
-  HL7_2_3_1, HL7_2_3_1_MSH,
-  HL7_2_4, HL7_2_4_MSH,
-  HL7_2_5, HL7_2_5_MSH,
-  HL7_2_5_1, HL7_2_5_1_MSH,
-  HL7_2_6, HL7_2_6_MSH,
-  HL7_2_7, HL7_2_7_MSH,
-  HL7_2_7_1, HL7_2_7_1_MSH,
-  HL7_2_8, HL7_2_8_MSH
-}
-
-export { HL7_SPEC, HL7_SPEC_BASE }
-
 export type { ClientOptions, ClientListenerOptions } from './utils/normalizedClient.js'
 export type { ClientBuilderFileOptions, ClientBuilderMessageOptions, ClientBuilderOptions } from './utils/normalizedBuilder.js'
 export type { HL7Error, HL7FatalError, HL7ParserError } from './utils/exception.js'
 
 export default Client
-export { Client, HL7Outbound, OutboundHandler, InboundResponse, FileBatch, Batch, Message, Segment, SegmentList, Component, SubComponent, Field, FieldRepetition, ParserPlan, Node, RootBase, NodeBase, ValueNode, EmptyNode, Delimiters, ReadyState }
+export { Client, HL7Outbound, OutboundHandler, InboundResponse, FileBatch, Batch, Message, ReadyState }
