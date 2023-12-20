3b81b029c3fee0f3d4c228190a6d8d30b1ec1228
Shane Froebel <bugs5382@users.noreply.github.com>
2023-12-19 21:13:30 -0500

    feat: update packages
    * updated unit tests
    * updated packages

--- __tests__/hl7.end2end.test.ts
+++ __tests__/hl7.end2end.test.ts
@@ -1,17 +1,127 @@
-/*
 import fs from "fs";
-import {Server} from "node-hl7-server";
 import path from "node:path";
 import portfinder from "portfinder";
-import {Batch, Client, Message} from "../src";
-import {sleep} from "./__utils__";
-*/
+import {Hl7Inbound, Server} from "node-hl7-server";
+import {Batch, Client, HL7_2_7_MSH, HL7Outbound, Message} from "../src";
+import {expectEvent, sleep} from "./__utils__";
+
+const MSH_HEADER: HL7_2_7_MSH = {
+  msh_9_1: "ADT",
+  msh_9_2: "A01",
+  msh_11_1: "D"
+}
 
 describe('node hl7 end to end', () => {
 
-  test.todo('hide all tests')
+  describe('server/client sanity checks', () => {
 
-  /*
+    let waitAck: number = 0
+
+    let server: Server
+    let listener: Hl7Inbound
+
+    let client: Client
+    let outGoing: HL7Outbound
+
+    beforeEach(async () => {
+
+      const LISTEN_PORT = await portfinder.getPortPromise({
+        port: 3000,
+        stopPort: 65353
+      })
+
+      server = new Server({bindAddress: '0.0.0.0'})
+      listener = server.createInbound({port: LISTEN_PORT}, async () => {})
+
+      client = new Client({host: '0.0.0.0'})
+      outGoing = client.createOutbound({port: LISTEN_PORT, waitAck: waitAck !== 2}, async () => {})
+
+    })
+
+    afterEach(async () => {
+      await outGoing.close()
+      await listener.close()
+
+      waitAck = waitAck + 1;
+    })
+
+    test('...simple connect', async () => {
+
+      const LISTEN_PORT = await portfinder.getPortPromise({
+        port: 3000,
+        stopPort: 65353
+      })
+
+      const server = new Server({ bindAddress: '0.0.0.0'})
+      const listener = server.createInbound({port: LISTEN_PORT}, async () => {})
+
+      const client = new Client({ host: '0.0.0.0'})
+      const outGoing = client.createOutbound({ port: LISTEN_PORT }, async () => {})
+
+      await expectEvent(listener, 'client.connect')
+      await expectEvent(outGoing, 'connect')
+
+      await outGoing.close()
+      await listener.close()
+
+    })
+
+    test('...send simple message, just to make sure it sends, no data checks', async () => {
+
+      let message = new Message({
+        messageHeader: {
+          ...MSH_HEADER,
+          msh_10: 'CONTROL_ID'
+        }
+      })
+
+      await outGoing.sendMessage(message)
+
+    })
+
+    test('...send simple message twice, fails because no ACK of the first', async () => {
+
+      try {
+        let message = new Message({
+          messageHeader: {
+            ...MSH_HEADER,
+            msh_10: 'CONTROL_ID'
+          }
+        })
+
+        await outGoing.sendMessage(message)
+        await outGoing.sendMessage(message)
+
+      } catch (err: any) {
+        expect(err.message).toBe(`Can't send message while we are waiting for a response.`)
+      }
+
+    })
+
+    // Note: For this test to pass, you must run it from the describe block!
+    test('...send simple message twice, no ACK needed', async () => {
+
+      let message = new Message({
+        messageHeader: {
+          ...MSH_HEADER,
+          msh_10: 'CONTROL_ID'
+        }
+      })
+
+      await outGoing.sendMessage(message)
+
+      message = new Message({
+        messageHeader: {
+          ...MSH_HEADER,
+          msh_10: 'CONTROL_ID'
+        }
+      })
+
+      await outGoing.sendMessage(message)
+
+    })
+
+  })
 
   describe('...send message, get proper ACK', () => {
 
@@ -132,7 +242,6 @@ describe('node hl7 end to end', () => {
       const client = new Client({host: '0.0.0.0'})
       const OB_ADT = client.createOutbound({ port: LISTEN_PORT }, async (res) => {
         const messageRes = res.getMessage()
-        console.log(messageRes.get('MSA.1').toString())
         expect(messageRes.get('MSA.1').toString()).toBe('AA')
       })
 
@@ -156,14 +265,8 @@ describe('node hl7 end to end', () => {
       message.set('MSH.7', '20081231')
 
       batch.add(message)
-      batch.add(message)
 
       batch.end()
-
-      const string = [`BHS|^~\\&|||||20081231`, `MSH|^~\\&|||||20081231||ADT^A01^ADT_A01|CONTROL_ID1|D|2.7`, `MSH|^~\\&|||||20081231||ADT^A01^ADT_A01|CONTROL_ID1|D|2.7`, "BTS|2"].join("\r")
-      expect(batch.toString()).toBe(string)
-
-      //console.log(batch.toString().split("\r").join("\n"))
 
       await OB_ADT.sendMessage(batch)
 
@@ -174,7 +277,7 @@ describe('node hl7 end to end', () => {
 
     })
 
-    test.skip('...tls', async () => {
+    test('...tls', async () => {
 
       const server = new Server(
         {
@@ -196,7 +299,8 @@ describe('node hl7 end to end', () => {
 
       const client = new Client({host: '0.0.0.0', tls: { rejectUnauthorized: false }})
       const OB_ADT = client.createOutbound({ port: LISTEN_PORT }, async (res) => {
-        expect(res.toString()).not.toContain('ADT^A01^ADT_A01')
+        const messageRes = res.getMessage()
+        expect(messageRes.get('MSA.1').toString()).toBe('AA')
       })
 
       await sleep(5)
@@ -227,8 +331,5 @@ describe('node hl7 end to end', () => {
     })
 
   })
-
-
-   */
 
 })
