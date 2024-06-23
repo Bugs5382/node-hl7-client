e0be38ff411122229d9dbf3acf7a864b7f59f229
Bugs5382 <bugs5382@users.noreply.github.com>
2024-06-23 07:37:27 -0400

    docs: updated docs [ci skip]

--- src/client/connection.ts
+++ src/client/connection.ts
@@ -14,7 +14,7 @@ import { Client } from './client.js'
 import { InboundResponse } from './module/inboundResponse.js'
 
 /* eslint-disable */
-export interface Connection extends EventEmitter {
+export interface IConnection extends EventEmitter {
   /** The connection has been closed manually. You have to start the connection again. */
   on(name: 'close', cb: () => void): this;
   /** The connection is made. */
@@ -37,7 +37,7 @@ export interface Connection extends EventEmitter {
 /** Connection Class
  * @description Create a connection customer that will listen to result send to the particular port.
  * @since 1.0.0 */
-export class Connection extends EventEmitter implements Connection {
+export class Connection extends EventEmitter implements IConnection {
   /** @internal */
   _handler: OutboundHandler
   /** @internal */
